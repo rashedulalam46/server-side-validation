667e1e12d8cf0234a1503242163a24da98b7f38a
Rashedul Alam <rashedulalam46@gmail.com>
2025-06-16 13:58:50 +0600

    some validation added

--- Models/User.cs
+++ Models/User.cs
@@ -7,8 +7,13 @@ namespace ServerSideValidation.Models
     {
         public int Id { get; set; }
         public string Username { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+
+        [Range(18, 100, ErrorMessage = "Age must be between 18 and 100.")]
         public int Age { get; set; }
         public string WebSite { get; set; }
         public string CreditCard { get; set; }
